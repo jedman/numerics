521e4075d6e5fcdfe4854facb9700f9e16b53b3f
Jacob Edman <jedman42@gmail.com>
2014-02-02 21:08:49 -0800

    almost done with problem 1 :(

--- src1/heat_solver.pyx
+++ src1/heat_solver.pyx
@@ -39,6 +39,13 @@ def heat_solver_full(hinv,kinv,init='sine'):
         u[i+1,0] = 0
         u[i+1,-1] = 0 
     return u
+def exact(x, init = 'sine'):
+    '''computes the exact solution to the heat equation at t = 1 for I.C. sin(pi x) and x(1-x) along the interval [0,1] '''
+    if init == 'sine': 
+        u = np.exp(-pi**2*1)*np.sin(pi*x)
+    if init == 'arc':
+        u = x*(1-x)
+    return u
 
 
 
